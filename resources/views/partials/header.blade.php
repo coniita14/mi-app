c252e19d50fa0f969df9677ca83463e7c464b83d
Constanza Castillo <conicastillo644@gmail.com>
2026-06-07 02:19:11 -0300

    Trabajo practico gestion laboratorio 2

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,5 +2,6 @@
     <nav>
         <a href="{{ route('inicio') }}">Inicio</a>
         <a href="{{ route('catalogo') }}">Catálogo</a>
+        <a href="{{ route('equipos.index') }}">Equipos</a>
     </nav>
 </header>
